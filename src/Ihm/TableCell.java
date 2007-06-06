81805d99b99dfa248974212b8ceee631c9709f99
zeta <zeta@35ac62f8-fa2b-0410-b5a9-7d4dd5ab9d41>
2007-06-06 21:53:22 +0000

    oubli du fichier TableCell.java

---
+++ src/Ihm/TableCell.java
@@ -0,0 +1,21 @@
+/*
+ * TableCell.java
+ *
+ * Created on 6 juin 2007, 23:22
+ *$Id$
+ */
+
+package Ihm;
+
+/**
+ *
+ * @author zeta
+ */
+public class  TableCell {
+    
+    /** Creates a new instance of TableCell */
+   public float v;
+   public boolean done; 
+   public TableCell(float _v,boolean _d){v=_v;done=_d;}
+   
+}
